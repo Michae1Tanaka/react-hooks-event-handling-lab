0da909f7d49dd5959f80ea810ed0c8eef43ec87b
Michael Tanaka <tanakam4214@gmail.com>
2023-05-28 13:06:29 -0700

    Lab Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const onFocusHandler = (e) => {
+    console.log("Good!");
+  };
+  const onBlurHandler = (e) => {
+    console.log("Hey! Eyes on me!");
+  };
+  return (
+    <button onFocus={onFocusHandler} onBlur={onBlurHandler}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const inputHandler = (event) => {
+    console.log("Entering password...");
+  };
+  return (
+    <form>
+      <input type="password" onChange={inputHandler}></input>
+    </form>
+  );
 }
 
 export default Keypad;
